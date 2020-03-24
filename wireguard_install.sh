7c2ddc2a865eed7cb8709c371b04d1c5a5fcdb80
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2020-03-24 18:29:45 +0800

    Update wireguard_install.sh
    change to 10.66

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #判断系统
+#content=$(cat /etc/wireguard/xxx.conf) && echo "${content}" | qrencode -o - -t UTF8
+#systemctl status wg-quick@wg0.service
+#dkms status
 if [ ! -e '/etc/redhat-release' ]; then
 echo "仅支持centos7"
 exit
@@ -61,7 +64,7 @@ config_client(){
 cat > /etc/wireguard/client.conf <<-EOF
 [Interface]
 PrivateKey = $c1
-Address = 10.0.0.2/24 
+Address = 10.66.0.2/24 
 DNS = 8.8.8.8
 MTU = 1420
 
@@ -112,7 +115,7 @@ wireguard_install(){
 cat > /etc/wireguard/wg0.conf <<-EOF
 [Interface]
 PrivateKey = $s1
-Address = 10.0.0.1/24 
+Address = 10.66.0.1/24 
 PostUp   = echo 1 > /proc/sys/net/ipv4/ip_forward; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
 PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $eth -j MASQUERADE
 ListenPort = $port
@@ -121,7 +124,7 @@ MTU = 1420
 
 [Peer]
 PublicKey = $c2
-AllowedIPs = 10.0.0.2/32
+AllowedIPs = 10.66.0.2/32
 EOF
 
     config_client
@@ -140,14 +143,14 @@ add_user(){
     ipnum=$(grep Allowed /etc/wireguard/wg0.conf | tail -1 | awk -F '[ ./]' '{print $6}')
     newnum=$((10#${ipnum}+1))
     sed -i 's%^PrivateKey.*$%'"PrivateKey = $(cat temprikey)"'%' $newname.conf
-    sed -i 's%^Address.*$%'"Address = 10.0.0.$newnum\/24"'%' $newname.conf
+    sed -i 's%^Address.*$%'"Address = 10.66.0.$newnum\/24"'%' $newname.conf
 
 cat >> /etc/wireguard/wg0.conf <<-EOF
 [Peer]
 PublicKey = $(cat tempubkey)
-AllowedIPs = 10.0.0.$newnum/32
+AllowedIPs = 10.66.0.$newnum/32
 EOF
-    wg set wg0 peer $(cat tempubkey) allowed-ips 10.0.0.$newnum/32
+    wg set wg0 peer $(cat tempubkey) allowed-ips 10.66.0.$newnum/32
     echo -e "\033[37;41m添加完成，文件：/etc/wireguard/$newname.conf\033[0m"
     rm -f temprikey tempubkey
 }
