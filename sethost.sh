931aa055b79b72b52165a19d41e8ddc56e5090cd
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2020-03-27 14:25:31 +0800

    Update sethost.sh

--- sethost.sh
+++ sethost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# bash sethost.sh xxx.net aa.xxx.net 80 passwd
+# bash sethost.sh xxx.net aa.xxx.net 80 "passwd"
 
 #Set Caddy 
 # /usr/local/caddy/Caddyfile
@@ -11,16 +11,16 @@ set_caddy_80()
 	rm -rf /usr/local/caddy/Caddyfile.bak_fuck
 	mv /usr/local/caddy/Caddyfile /usr/local/caddy/Caddyfile.bak_fuck
 	rm -rf /usr/local/caddy/Caddyfile
-	cat <<EOF>  /usr/local/caddy/Caddyfile
+	cat > /usr/local/caddy/Caddyfile  << EOF
 		:443 {
 			root /usr/local/caddy/www
 			timeouts none
-			tls /root/ssl/{$domain}/cert.pem /root/ssl/{$domain}}/privkey.pem
+			tls /root/ssl/${domain}/cert.pem /root/ssl/${domain}/privkey.pem
 			gzip
 		}
 
 		:8099 {
-			redir https://{$subdomain}{uri}
+			redir https://${subdomain}{uri}
 		}
 EOF
 
@@ -33,16 +33,16 @@ set_caddy_443()
 	rm -rf /usr/local/caddy/Caddyfile.bak_fuck
 	mv /usr/local/caddy/Caddyfile /usr/local/caddy/Caddyfile.bak_fuck
 	rm -rf /usr/local/caddy/Caddyfile
-	cat <<EOF>  /usr/local/caddy/Caddyfile
+	cat > /usr/local/caddy/Caddyfile  << EOF
 		:8100 {
 			root /usr/local/caddy/www
 			timeouts none
-			tls /root/ssl/{$domain}/cert.pem /root/ssl/{$domain}}/privkey.pem
+			tls /root/ssl/${domain}/cert.pem /root/ssl/${domain}/privkey.pem
 			gzip
 		}
 
 		:80 {
-			redir https://{$subdomain}{uri}
+			redir https://${subdomain}{uri}
 		}
 EOF
 
@@ -57,14 +57,14 @@ set_config_80()
 	rm -rf /etc/shadowsocks.json.bak_fuck
 	mv /etc/shadowsocks.json /etc/shadowsocks.json.bak_fuck
 	rm -rf /etc/shadowsocks.json
-	cat <<EOF>  /etc/shadowsocks.json
+	cat > /etc/shadowsocks.json  << EOF
 	{
 	    "server":"0.0.0.0",
 	    "server_ipv6":"[::]",
 	    "server_port":80,
 	    "local_address":"127.0.0.1",
 	    "local_port":1080,
-	    "password":"$pwd",
+	    "password":"${pwd}",
 	    "timeout":120,
 	    "method":"none",
 	    "protocol":"auth_chain_a",
@@ -86,14 +86,14 @@ set_config_443()
 	rm -rf /etc/shadowsocks.json.bak_fuck
 	mv /etc/shadowsocks.json /etc/shadowsocks.json.bak_fuck
 	rm -rf /etc/shadowsocks.json
-	cat <<EOF>  /etc/shadowsocks.json
+	cat > /etc/shadowsocks.json << EOF
 	{
 	    "server":"0.0.0.0",
 	    "server_ipv6":"[::]",
 	    "server_port":443,
 	    "local_address":"127.0.0.1",
 	    "local_port":1080,
-	    "password":"$pwd",
+	    "password":"${pwd}",
 	    "timeout":120,
 	    "method":"none",
 	    "protocol":"auth_chain_a",
@@ -113,6 +113,7 @@ domain=$1
 subdomain=$2
 method=$3
 pwd=$4
+
 
 [ -z $1 ] && [ -z $2 ] && [ -z $3 ] && [ -z $4 ]
 case "$method" in
