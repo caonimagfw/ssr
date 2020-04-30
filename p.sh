2406c3628e311c6d7d03ff636618ed8139e44aa8
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2020-04-30 21:15:39 +0800

    Update p.sh

--- p.sh
+++ p.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #get file 
-wget --no-check-certificate https://raw.githubusercontent.com/caonimagfw/shadowsocks/master/shadowsocksR-debian p-linux64.tar.bz2
+wget --no-check-certificate https://github.com/caonimagfw/ssr/raw/master/p-linux64.tar.bz2
 tar -xf p-linux64.tar.bz2
 cd p-linux64
 mv pypy2.7-v7.3.1-linux64 pypy
