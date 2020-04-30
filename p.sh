ef18f2aab1223eb8fceb7958376f4375836c7bc8
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2020-04-30 21:24:46 +0800

    Update p.sh

--- p.sh
+++ p.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
+#
+yum install bzip2
 #get file 
 wget --no-check-certificate https://github.com/caonimagfw/ssr/raw/master/p-linux64.tar.bz2
 tar -xf p-linux64.tar.bz2
-cd p-linux64
 mv pypy2.7-v7.3.1-linux64 pypy
 mv pypy /usr/local/
 
 #create link 
-ln -s  /usr/local/pypy/bin/pypy /usr/bin/pypy
+if [ ! -d "/usr/bin/pypy" ]; then
+	ln -s  /usr/local/pypy/bin/pypy /usr/bin/pypy
+fi
